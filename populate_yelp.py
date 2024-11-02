2001b1c4175bc5f06113317e96b028cbc1fde511
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2024-11-02 11:35:08 +0100

    populate yelp database

--- populate_yelp.py
+++ populate_yelp.py
@@ -1,3 +1,30 @@
+import json
 import duckdb
 
 connection = duckdb.connect('yelp.db')
+
+# Create table if it does not exist
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS users (
+    data json
+    );
+'''
+
+connection.execute(create_table_query)
+
+
+def import_json(obj):
+    # Convert JSON object to a JSON string
+    import_json_query = '''
+    INSERT INTO users (data) VALUES (?);
+    '''
+    connection.execute(import_json_query, [json.dumps(obj)])
+
+
+with open('./data/yelp/yelp_academic_dataset_user.json', 'r') as file:
+    for line in file:
+        try:
+            json_obj = json.loads(line)
+            import_json(json_obj)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON on line: {e}")
